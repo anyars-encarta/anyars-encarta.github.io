53cf5df18b6e2d76b87d65b6f65a780a64602dc2
Mozamel Jawad <mozameljawad@gmail.com>
2023-06-27 12:16:30 +0430

    Update app.js
    add some changes in app js file

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ const hamburger = document.querySelector(".hamburger");
      navMenu.classList.toggle("active");
  })
 
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+     hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+ }))
  
