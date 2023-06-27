97b8951b1c03c1f905b3735b3b962481c86a9e19
anyars-encarta <anyarsencarta@gmail.com>
2023-06-27 07:44:28 +0000

    Updated CSS and Javascript

--- app.js
+++ app.js
@@ -6,27 +6,4 @@ const hamburger = document.querySelector(".hamburger");
      navMenu.classList.toggle("active");
  })
 
- document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-     hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
- }))
-
- const sections = document.querySelectorAll("section");
- const navlist = document.querySelectorAll("header .navbar ul li");
-
- window.addEventListener("scroll", () => {
-    let current = "";
-    sections.forEach( section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if(pageYOffset > sectionTop) {
-            current = section.getAttribute("id");
-        }
-    })
-    navlist.forEach( li => {
-        li.classList.remove("active");
-        if(li.classList.contains("current")){
-            li.classList.add("active")
-        }
-    })
- })
+ 
