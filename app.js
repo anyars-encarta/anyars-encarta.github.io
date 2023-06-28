b28f0a3a4b2d84b00a83a8d1f695b527ce64dc5e
anyars-encarta <anyarsencarta@gmail.com>
2023-06-28 21:49:51 +0000

    Updated Popup window feature

--- app.js
+++ app.js
@@ -38,16 +38,30 @@ form.addEventListener('submit', (e) => {
     errorElement.innerText = '';
   }
 });
-    
+
 // JS for Creating Dynamic HTML
-const popupDiv = document.createElement('div');
+let popupDiv = document.createElement('div');
 popupDiv.classList.add('modal');
 popupDiv.id = 'modal';
 
 document.body.appendChild(popupDiv);
 
-const div = document.createElement('div');
-div.id = 'overlay';
+// document.getElementById('print-html').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-1').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-2').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-3').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-4').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-5').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-6').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-7').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-8').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-9').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-10').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-11').addEventListener('click', addPopupDetails);
+document.querySelector('.btn-12').addEventListener('click', addPopupDetails);
+
+let div = document.createElement('div');
+div.id = 'overlay'
 document.body.appendChild(div);
 
 function addPopupDetails() {
@@ -57,7 +71,7 @@ function addPopupDetails() {
       name: document.getElementById('name-1').value,
       projectDescription: document.getElementById('desc-1').value,
       featuredImage: document.getElementById('img-1').value,
-      Technologies: {HTML: document.getElementById('html-1').value, 
+      technologies: {HTML: document.getElementById('html-1').value, 
                      CSS: document.getElementById('css-1').value, 
                      JavaScript: document.getElementById('js-1').value
                     },
@@ -69,7 +83,7 @@ function addPopupDetails() {
       name: document.getElementById('name-2').value,
       projectDescription: document.getElementById('desc-2').value,
       featuredImage: document.getElementById('img-2').value,
-      Technologies: {HTML: document.getElementById('html-2').value, 
+      technologies: {HTML: document.getElementById('html-2').value, 
                      CSS: document.getElementById('css-2').value, 
                      JavaScript: document.getElementById('js-2').value
                     },
@@ -81,7 +95,7 @@ function addPopupDetails() {
       name: document.getElementById('name-3').value,
       projectDescription: document.getElementById('desc-3').value,
       featuredImage: document.getElementById('img-3').value,
-      Technologies: {HTML: document.getElementById('html-3').value, 
+      technologies: {HTML: document.getElementById('html-3').value, 
                      CSS: document.getElementById('css-3').value, 
                      JavaScript: document.getElementById('js-3').value
                     },
@@ -93,7 +107,7 @@ function addPopupDetails() {
       name: document.getElementById('name-4').value,
       projectDescription: document.getElementById('desc-4').value,
       featuredImage: document.getElementById('img-4').value,
-      Technologies: {HTML: document.getElementById('html-4').value, 
+      technologies: {HTML: document.getElementById('html-4').value, 
                      CSS: document.getElementById('css-4').value, 
                      JavaScript: document.getElementById('js-4').value
                     },
@@ -105,7 +119,7 @@ function addPopupDetails() {
       name: document.getElementById('name-5').value,
       projectDescription: document.getElementById('desc-5').value,
       featuredImage: document.getElementById('img-5').value,
-      Technologies: {HTML: document.getElementById('html-5').value, 
+      technologies: {HTML: document.getElementById('html-5').value, 
                      CSS: document.getElementById('css-5').value, 
                      JavaScript: document.getElementById('js-5').value
                     },
@@ -117,7 +131,7 @@ function addPopupDetails() {
       name: document.getElementById('name-6').value,
       projectDescription: document.getElementById('desc-6').value,
       featuredImage: document.getElementById('img-6').value,
-      Technologies: {HTML: document.getElementById('html-6').value, 
+      technologies: {HTML: document.getElementById('html-6').value, 
                      CSS: document.getElementById('css-6').value, 
                      JavaScript: document.getElementById('js-6').value
                     },
@@ -129,7 +143,7 @@ function addPopupDetails() {
       name: document.getElementById('name-7').value,
       projectDescription: document.getElementById('desc-7').value,
       featuredImage: document.getElementById('img-7').value,
-      Technologies: {HTML: document.getElementById('html-7').value, 
+      technologies: {HTML: document.getElementById('html-7').value, 
                      CSS: document.getElementById('css-7').value, 
                      JavaScript: document.getElementById('js-7').value
                     },
@@ -141,7 +155,7 @@ function addPopupDetails() {
       name: document.getElementById('name-8').value,
       projectDescription: document.getElementById('desc-8').value,
       featuredImage: document.getElementById('img-8').value,
-      Technologies: {HTML: document.getElementById('html-8').value, 
+      technologies: {HTML: document.getElementById('html-8').value, 
                      CSS: document.getElementById('css-8').value, 
                      JavaScript: document.getElementById('js-8').value
                     },
@@ -153,7 +167,7 @@ function addPopupDetails() {
       name: document.getElementById('name-9').value,
       projectDescription: document.getElementById('desc-9').value,
       featuredImage: document.getElementById('img-9').value,
-      Technologies: {HTML: document.getElementById('html-9').value, 
+      technologies: {HTML: document.getElementById('html-9').value, 
                      CSS: document.getElementById('css-9').value, 
                      JavaScript: document.getElementById('js-9').value
                     },
@@ -165,7 +179,7 @@ function addPopupDetails() {
       name: document.getElementById('name-10').value,
       projectDescription: document.getElementById('desc-10').value,
       featuredImage: document.getElementById('img-10').value,
-      Technologies: {HTML: document.getElementById('html-10').value, 
+      technologies: {HTML: document.getElementById('html-10').value, 
                      CSS: document.getElementById('css-10').value, 
                      JavaScript: document.getElementById('js-10').value
                     },
@@ -177,7 +191,7 @@ function addPopupDetails() {
       name: document.getElementById('name-11').value,
       projectDescription: document.getElementById('desc-11').value,
       featuredImage: document.getElementById('img-11').value,
-      Technologies: {HTML: document.getElementById('html-11').value, 
+      technologies: {HTML: document.getElementById('html-11').value, 
                      CSS: document.getElementById('css-11').value, 
                      JavaScript: document.getElementById('js-11').value
                     },
@@ -189,7 +203,7 @@ function addPopupDetails() {
       name: document.getElementById('name-12').value,
       projectDescription: document.getElementById('desc-12').value,
       featuredImage: document.getElementById('img-12').value,
-      Technologies: {HTML: document.getElementById('html-12').value, 
+      technologies: {HTML: document.getElementById('html-12').value, 
                      CSS: document.getElementById('css-12').value, 
                      JavaScript: document.getElementById('js-12').value
                     },
@@ -197,51 +211,42 @@ function addPopupDetails() {
       linkToSource: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
     },
     ];
-      
-  projects.forEach((project, index) => {
-    function addPopupDetails() {
-      let popupTitle = document.getElementById('print-1').value
-      let popupImage = 'logos/Popup-Portfolio-Big.png';
-      let popupImageAltText = document.querySelector('[alt]');
-      let projectDescription = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> </br>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.';
+  
+    projects.forEach((project, index) => {
+      let div = document.createElement('div');
+      div.id = 'overlay';
+      document.body.appendChild(div);
       
       let popupFormDetails = `<div class="modal-header">
-      <div class="title">Keeping track of hundreds of components website</div>
+      <div class="title">${project.name}</div>
       <button data-close-button class="close-button">&times;</button>
    </div>
    
    <div class="modal-body">
       <ul class="popup-list" id="detail-id">
-          <li class="list-item" id="item-1"><a href="#">HTML</a></li>
-          <li class="list-item" id="item-1"><a href="#">Bootstrap</a></li>
-          <li class="list-item" id="item-1"><a href="#">Ruby on rails</a></li>
+          <li class="list-item" id="item-1"><a href="#">${project.technologies.HTML}</a></li>
+          <li class="list-item" id="item-1"><a href="#">${project.technologies.CSS}</a></li>
+          <li class="list-item" id="item-1"><a href="#">${project.technologies.JavaScript}</a></li>
       </ul>
    
       <div class="middle-section">
-        <img src="./logos/Popup-Portfolio-Big.png" alt="Snapshoot Portfolio">
+        <img src=${project.featuredImage} alt="Snapshoot Portfolio">
         
         <div class="side-items">
-          <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting 
-              industry. Lorem Ipsum has been the industry's 
-              standard dummy text ever since the 1500s, when an 
-              unknown printer took a galley of type and scrambled it to make a 
-              type specimen book. It has survived not only five centuries, but 
-              also the leap into electronic typesetting, remaining essent
-          </p>
+          <p>${project.projectDescription}</p>
           <div class="popup-buttons">
-              <a href ="#" class="button-1" id="btn-1">See more<img src="./logos/Icon-with-arrow.png" alt=""></a>
-              <a href ="#" class="button-2" id="btn-2">See Source<img src="./logos/New-Github.png" alt=""></a>
+              <a href ="https://anyars-encarta.github.io/" class="button-1" id="btn-1">See more<img src="./logos/Icon-with-arrow.png" alt=""></a>
+              <a href ="https://github.com/anyars-encarta/anyars-encarta.github.io" class="button-2" id="btn-2">See Source<img src="./logos/New-Github.png" alt=""></a>
           </div>
         </div>
       </div>
-   </div>`
-   
-   popupDiv.insertAdjacentHTML('beforeend', popupFormDetails)
-   }
-  })
+  </div>`;
+    
+  popupDiv.insertAdjacentHTML('beforeend', popupFormDetails);
+  });
+}
 
-// JS for Popup Window
+    // JS for Popup Window
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
@@ -284,3 +289,4 @@ closeModalButtons.forEach((button) => {
   const div = document.querySelector('modal');
   document.body.removeChild(div);
 });
+
