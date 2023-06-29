6457e1da74c0ce572d804ac552884836d04768dc
anyars-encarta <anyarsencarta@gmail.com>
2023-06-29 06:28:46 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// JS for Hamburger Menu
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
@@ -10,3 +11,29 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+// JS for Form Validation
+const name = document.getElementById('fullname');
+const email = document.getElementById('email-address');
+const form = document.getElementById('contact');
+const errorElement = document.getElementById('small');
+
+form.addEventListener('submit', (e) => {
+  if (name.value === '' || name.value == null) {
+    e.preventDefault();
+    errorElement.innerText = 'Please enter your name';
+  } else if (email.value === '' || email.value == null) {
+    e.preventDefault();
+    errorElement.innerText = 'Please enter email';
+  } else if (email.value === email.value.toUpperCase()) {
+    e.preventDefault();
+    errorElement.innerText = 'Please use Lowercase';
+  } else if (/[A-Z]/.test(email.value)) {
+    e.preventDefault();
+    document.getElementById('email-address').style.color = 'blue';
+    errorElement.innerText = 'Please use Lowercase';
+  } else {
+    document.getElementById('email-address').style.color = 'grey';
+    errorElement.innerText = '';
+  }
+});
