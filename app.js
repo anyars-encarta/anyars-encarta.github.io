9ffe245863d45db42857bb1e5aee1c4d6996323c
anyars-encarta <anyarsencarta@gmail.com>
2023-07-01 01:44:37 +0000

    Correct linter errors

--- app.js
+++ app.js
@@ -93,8 +93,8 @@ const data = [
     featuredImage: './logos/Img Placeholder 3.png',
     liveLink: 'https://anyars-encarta.github.io/',
     sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
-  },
-  {
+},
+{
     id: 4,
     title: 'Profesional Art Printing Data',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
@@ -102,8 +102,8 @@ const data = [
     featuredImage: './logos/Img Placeholder 1.png',
     liveLink: 'https://anyars-encarta.github.io/',
     sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
-  },
-  {
+},
+{
     id: 5,
     title: 'Profesional Art Printing Data',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
@@ -111,8 +111,8 @@ const data = [
     featuredImage: './logos/Img Placeholder 1.png',
     liveLink: 'https://anyars-encarta.github.io/',
     sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
-  },
-  {
+},
+{
     id: 6,
     title: 'Profesional Art Printing Data',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
@@ -120,8 +120,8 @@ const data = [
     featuredImage: './logos/Img Placeholder 1.png',
     liveLink: 'https://anyars-encarta.github.io/',
     sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
-  },
-  {
+},
+{
     id: 7,
     title: 'Profesional Art Printing Data',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
@@ -129,8 +129,8 @@ const data = [
     featuredImage: './logos/Img Placeholder 1.png',
     liveLink: 'https://anyars-encarta.github.io/',
     sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
-  },
-  {
+},
+{
     id: 8,
     title: 'Profesional Art Printing Data',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
@@ -138,8 +138,8 @@ const data = [
     featuredImage: './logos/Img Placeholder 1.png',
     liveLink: 'https://anyars-encarta.github.io/',
     sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
-  },
-  {
+},
+{
     id: 9,
     title: 'Profesional Art Printing Data',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
@@ -147,8 +147,8 @@ const data = [
     featuredImage: './logos/Img Placeholder 1.png',
     liveLink: 'https://anyars-encarta.github.io/',
     sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
-  },
-  {
+},
+{
     id: 10,
     title: 'Profesional Art Printing Data',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
@@ -156,8 +156,8 @@ const data = [
     featuredImage: './logos/Img Placeholder 1.png',
     liveLink: 'https://anyars-encarta.github.io/',
     sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
-  },
-  {
+},
+{
     id: 11,
     title: 'Data Dashboard Healthcare',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
@@ -165,32 +165,36 @@ const data = [
     featuredImage: './logos/Img Placeholder 2.png',
     liveLink: 'https://anyars-encarta.github.io/',
     sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
-  },
-  {
-    id: 12,
-    title: 'Website Portfolio',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
-    languages: ['HTML', 'CSS', 'JavaScript'],
-    featuredImage: './logos/Img Placeholder 3.png',
-    liveLink: 'https://anyars-encarta.github.io/',
-    sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
-  }];
+},
+{
+   id: 12,
+   title: 'Website Portfolio',
+   description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
+   languages: ['HTML', 'CSS', 'JavaScript'],
+   featuredImage: './logos/Img Placeholder 3.png',
+   liveLink: 'https://anyars-encarta.github.io/',
+   sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
+}];
 
+const popupWin = document.body;
 const cards = document.querySelector('.extra');
 const modalWarpper = document.querySelector('#modal-wrapper');
+const modalBox = document.querySelector('#modal-box');
 
-function sliceText(text) {
-  const maxLength = 200;
-  if (text.length < maxLength) {
-    return text;
-  }
-  return text.slice(0, text.length - maxLength);
-}
+document.addEventListener('DOMContentLoaded', addCardsToUI);
 
-function addCardsToUI() {
-  const extraContainer = document.querySelector('.extra');
+  cards.addEventListener('click', (e) => {
+    if(e.target.classList.contains('see-btn')) {
+      const dataId = e.target.dataset.id;
+      addToModal(dataId);
+      showModal();
+    }
+  });
 
-  const fullWidthCardDisplay = `<article class="works-centered container">
+  function addCardsToUI() {
+    const extraContainer = document.querySelector('.extra');
+
+    const fullWidthCardDisplay = `<article class="works-centered container">
     <div class="main-works">
       <img src='${data[0].featuredImage}' alt="My Recent Works">
       <div class="main-works-1">
@@ -209,62 +213,11 @@ function addCardsToUI() {
     </div>
   </article>`;
 
-  document.addEventListener('DOMContentLoaded', addCardsToUI);
-
-  function removeModal() {
-    const firstSeg = modalWarpper.parentElement.firstElementChild.nextElementSibling
-    const secondSeg = nextElementSibling.firstElementChild.firstElementChild.lastElementChild;
-    const closeButton = firstSeg.secondSeg
-    closeButton.addEventListener('click', () => {
-      modalWarpper.classList.remove('active');
-    });
-  }
-  
-  function showModal() {
-    modalWarpper.classList.add('active');
-    removeModal();
-  }
-
-  function addToModal(dataId) {
-    const selectedData = data.filter((x) => x.id === +dataId);
-  
-    const modalTemplate = `<div id="modal-box">
-      <div id="modal-header">
-        <h2>${selectedData[0].title}</h2>
-        <button data-action="close" class="close-modal" id="close-modal">&times;</button>
-      </div>
-      <ul class="popup-lang">
-        <li><a href="#">${selectedData[0].languages[0]}</a></li>
-        <li><a href="#">${selectedData[0].languages[1]}</a></li>
-        <li><a href="#">${selectedData[0].languages[2]}</a></li>
-      </ul>
-      <div class="popup-details">
-        <img class="feature-image" src='${selectedData[0].featuredImage}' alt="Feature Portfolio Image">
-        <div class="side-info">
-          <p>${selectedData[0].description}</p>
-          <div class="out-links">
-            <a class="see-live" href=${selectedData[0].liveLink}>See Live<img src="./logos/See-Live.png" alt="live icon"></a>
-            <a class="see-source" href=${selectedData[0].sourceLink}>See Source<img src="./logos/See-Source.png" alt="source icon"></a>
-          </div>
-          </div>
-        </div>
-        </div>`;
-  
-    modalWarpper.innerHTML = modalTemplate;
-  }
-  
-  cards.addEventListener('click', (e) => {
-    if (e.target.classList.contains('see-btn')) {
-      const dataId = e.target.dataset.id;
-      addToModal(dataId);
-      showModal();
-    }
-  });
-  extraContainer.innerHTML = `
-  <div class="works-centered">${fullWidthCardDisplay}</div>
-  <div class="print-container container">
-  ${data.map((item, index) => (`<article style=" background: linear-gradient(to bottom, transparent, rgba(0, 0, 0, .9)), url('${item.featuredImage}') center/cover no-repeat;" class="card">
-  <div class="card-details">
+extraContainer.innerHTML = `
+<div class="works-centered">${fullWidthCardDisplay}</div>
+<div class="print-container container">
+${data.map((item, index) => (`<article style=" background: linear-gradient(to bottom, transparent, rgba(0, 0, 0, .9)), url('${item.featuredImage}') center/cover no-repeat;" class="card">
+<div class="card-details">
   <h4 class="card-title">${item.title}</h4>
   <p class="card-description">${sliceText(item.description)}...</p>
   <div class="card-tags">
@@ -272,8 +225,58 @@ function addCardsToUI() {
     <span>${item.languages[1]}</span>
     <span>${item.languages[2]}</span>
   </div>
-  </div>
-  <button class="card-btn see-btn" data-id="${item.id}">See Project</button>
-  </article>`)).join(' ')}
-  </div>`;
-}
+</div>
+<button class="card-btn see-btn" data-id="${item.id}">See Project</button>
+</article>`)).join(' ')}
+</div>`;
+  };
+
+  function addToModal(dataId) {
+    const selectedData = data.filter(x => x.id === +dataId);
+    console.log(selectedData[0].sourceLink);
+
+    const modalTemplate = `<div id="modal-box">
+    <div id="modal-header">
+      <h2>${selectedData[0].title}</h2>
+      <button data-action="close" class="close-modal" id="close-modal">&times;</button>
+    </div>
+    <ul class="popup-lang">
+      <li><a href="#">${selectedData[0].languages[0]}</a></li>
+      <li><a href="#">${selectedData[0].languages[1]}</a></li>
+      <li><a href="#">${selectedData[0].languages[2]}</a></li>
+    </ul>
+    <div class="popup-details">
+      <img class="feature-image" src='${selectedData[0].featuredImage}' alt="Feature Portfolio Image">
+      <div class="side-info">
+        <p>${selectedData[0].description}</p>
+        <div class="out-links">
+          <a class="see-live" href=${selectedData[0].liveLink}>See Live<img src="./logos/See-Live.png" alt="live icon"></a>
+          <a class="see-source" href=${selectedData[0].sourceLink}>See Source<img src="./logos/See-Source.png" alt="source icon"></a>
+        </div>
+        </div>
+      </div>
+      </div>`;
+
+  modalWarpper.innerHTML = modalTemplate;
+  };
+
+ function showModal() {
+   modalWarpper.classList.add('active');
+   removeModal();
+};
+
+ function removeModal() {
+   const parent = modalWarpper.parentElement.firstElementChild.nextElementSibling;
+   const closeButton = parent.nextElementSibling.firstElementChild.firstElementChild.lastElementChild;
+     closeButton.addEventListener('click', () => {
+      modalWarpper.classList.remove('active');
+     });
+   };
+
+   function sliceText(text) {
+    const maxLength = 200;
+    if (text.length < maxLength) {
+      return text;
+    }
+    return text.slice(0, text.length - maxLength);
+   };
