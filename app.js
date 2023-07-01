47c52d989faf0a431cb1bd90f6818f47e8712c5b
anyars-encarta <anyarsencarta@gmail.com>
2023-07-01 01:08:35 +0000

    Correct eslint errors

--- app.js
+++ app.js
@@ -167,22 +167,22 @@ const data = [
     sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
   },
   {
-   id: 12,
-   title: 'Website Portfolio',
-   description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
-   languages: ['HTML', 'CSS', 'JavaScript'],
-   featuredImage: './logos/Img Placeholder 3.png',
-   liveLink: 'https://anyars-encarta.github.io/',
-   sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
+     id: 12,
+     title: 'Website Portfolio',
+     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex voluptas nemo sequi eius beatae magni recusandae, obcaecati numquam nihil ipsam voluptatum nam vero...',
+     languages: ['HTML', 'CSS', 'JavaScript'],
+     featuredImage: './logos/Img Placeholder 3.png',
+     liveLink: 'https://anyars-encarta.github.io/',
+     sourceLink: 'https://github.com/anyars-encarta/anyars-encarta.github.io',
   }];
 
 const cards = document.querySelector('.extra');
 const modalWarpper = document.querySelector('#modal-wrapper');
 
   function addCardsToUI () {
-    const extraContainer = document.querySelector('.extra');
+  const extraContainer = document.querySelector('.extra');
 
-    const fullWidthCardDisplay = `<article class="works-centered container">
+  const fullWidthCardDisplay = `<article class="works-centered container">
     <div class="main-works">
       <img src='${data[0].featuredImage}' alt="My Recent Works">
       <div class="main-works-1">
@@ -204,18 +204,17 @@ const modalWarpper = document.querySelector('#modal-wrapper');
   document.addEventListener('DOMContentLoaded', addCardsToUI);
 
   cards.addEventListener('click', (e) => {
-    if(e.target.classList.contains('see-btn')) {
+    if (e.target.classList.contains('see-btn')) {
       const dataId = e.target.dataset.id;
       addToModal(dataId);
       showModal();
     }
   });
-  
-extraContainer.innerHTML = `
-<div class="works-centered">${fullWidthCardDisplay}</div>
-<div class="print-container container">
-${data.map((item, index) => (`<article style=" background: linear-gradient(to bottom, transparent, rgba(0, 0, 0, .9)), url('${item.featuredImage}') center/cover no-repeat;" class="card">
-<div class="card-details">
+  extraContainer.innerHTML = `
+  <div class="works-centered">${fullWidthCardDisplay}</div>
+  <div class="print-container container">
+  ${data.map((item, index) => (`<article style=" background: linear-gradient(to bottom, transparent, rgba(0, 0, 0, .9)), url('${item.featuredImage}') center/cover no-repeat;" class="card">
+  <div class="card-details">
   <h4 class="card-title">${item.title}</h4>
   <p class="card-description">${sliceText(item.description)}...</p>
   <div class="card-tags">
@@ -223,15 +222,14 @@ ${data.map((item, index) => (`<article style=" background: linear-gradient(to bo
     <span>${item.languages[1]}</span>
     <span>${item.languages[2]}</span>
   </div>
-</div>
-<button class="card-btn see-btn" data-id="${item.id}">See Project</button>
-</article>`)).join(' ')}
-</div>`;
+  </div>
+  <button class="card-btn see-btn" data-id="${item.id}">See Project</button>
+  </article>`)).join(' ')}
+  </div>`;
   };
 
-  function addToModal (dataId) {
-    const selectedData = data.filter(x => x.id === +dataId);
-    console.log(selectedData[0].sourceLink);
+function addToModal (dataId) {
+const selectedData = data.filter((x) => x.id === +dataId);
 
     const modalTemplate = `<div id="modal-box">
     <div id="modal-header">
@@ -258,22 +256,22 @@ ${data.map((item, index) => (`<article style=" background: linear-gradient(to bo
   modalWarpper.innerHTML = modalTemplate;
   };
 
- function showModal() {
-   modalWarpper.classList.add('active');
-   removeModal();
+function showModal() {
+  modalWarpper.classList.add('active');
+  removeModal()
 };
 
  function removeModal () {
    const closeButton = modalWarpper.parentElement.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.firstElementChild.lastElementChild;
-     closeButton.addEventListener('click', () => {
-      modalWarpper.classList.remove('active');
-     });
-   };
+   closeButton.addEventListener('click', () => {
+    modalWarpper.classList.remove('active');
+  })
+  };
 
-   function sliceText(text) {
-    const maxLength = 200;
-    if (text.length < maxLength) {
-      return text;
-    }
-    return text.slice(0, text.length - maxLength);
-   };
+  function sliceText(text) {
+  const maxLength = 200;
+  if (text.length < maxLength) {
+      return text
+  }
+  return text.slice(0, text.length - maxLength);
+  };
