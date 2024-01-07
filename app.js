fc1c58ec7e30ae32f3f77e3205f73aeb2136adb8
Anyars Yussif <anyarsencarta@gmail.com>
2024-01-07 13:42:23 +0000

    Update app.js

--- app.js
+++ app.js
@@ -201,7 +201,7 @@ ${data.map((item) => (`<article style=" background: linear-gradient(to bottom, t
 }
 
 document.addEventListener('DOMContentLoaded', addCardsToUI);
-// Add 
+// 
 function addToModal(dataId) {
   const selectedData = data.filter((x) => x.id === +dataId);
 
